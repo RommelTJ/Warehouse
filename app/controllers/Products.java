d5debeffe938acac3e4323748d656ecdc8a7ff7a
Rommel Rico <rommeltj@gmail.com>
2014-06-24 00:22:44 -0700

    Details view

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -3,17 +3,22 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.products.list;
+import views.html.products.details;
 import java.util.List;
 import models.Product;
+import play.data.Form;
 
 public class Products extends Controller {
+
+    private static final Form<Product> productForm = Form.form(Product.class);
+
     public static Result list() {
         List<Product> products = Product.findAll();
         return ok(list.render(products));
     }
 
     public static Result newProduct(){
-        return TODO;
+        return ok(details.render(productForm));
     }
 
     public static Result details(String ean){
