927d0b9981ea9019a64120da7d6792072c613016
Rommel Rico <rommeltj@gmail.com>
2014-07-27 17:27:03 -0700

    Ch12

---
+++ app/models/User.java
@@ -0,0 +1,46 @@
+package models;
+
+
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+public class User extends Model {
+
+    @Id
+    public Long id;
+    @Constraints.Required
+    public String email;
+    @Constraints.Required
+    public String password;
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public static User authenticate(String email,
+                                    String password) {
+// Should be something like
+// return finder.where().eq("email", email)
+// .eq("password", password).findUnique();
+        if ("nicolas".equals(email) && "nicolas".equals(password))
+            return new User("nicolas", "nicolas");
+        else
+            return null;
+    }
+
+    public static Finder<Long, User> finder
+            = new Finder<Long, User>(Long.class, User.class);
+
+}
--- build.sbt
+++ build.sbt
@@ -5,7 +5,13 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
-  cache
-)     
+  cache,
+  "com.google.guava" % "guava" % "14.0",
+  filters,
+  "securesocial" %% "securesocial" % "master-SNAPSHOT"
+)
+
+resolvers += Resolver.url("sbt-plugin-snapshots", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
+
 
 play.Project.playJavaSettings
